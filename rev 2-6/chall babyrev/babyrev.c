46822ba8c5afc20cf41f97cf9311ad1ff44ecee4
shivam joshi <53056412+TtvTooSmart@users.noreply.github.com>
2022-06-29 01:13:30 +0530

    added challenges

---
+++ rev 2-6/chall babyrev/babyrev.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "b64.h"
+
+
+int main(){
+char input[20],a[10]="AAAA";
+int x;
+printf("give me a input\n");
+scanf("%s", input);
+// printf("%s",input);
+x=strcmp(input,a);
+if(x==0)
+{
+
+
+    unsigned char *str = "TnV2Q1RGe2cwMGRfajBiX3kwdV9uMHdfdW4zcnN0NG5kX2I0czFjNV8wZl9yM3YzcnMzXzNuZzFuMzNyMW5nfQ==";
+    char *dec = b64_decode(str, strlen(str));
+    printf("%s\n", dec); 
+    free(dec);
+    return 0;
+
+    FILE *fp;
+    fp = fopen ("flag.txt", "w+");
+    fputs(dec, fp);
+    // fputs("NuvCTF{", fp); 
+    // fputs("g00d_j", fp);
+    // fputs("0b_y0u", fp);
+    // fputs("_n0w_un3rst", fp);
+    // fputs("4nd_b4s", fp);
+    // fputs("1c5_0f_r3v", fp);
+    // fputs("3rs3_3ng1n", fp);
+    // fputs("33r1ng}", fp);
+   
+}
+else
+{
+    return 0;
+}
+}
