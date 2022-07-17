c9c5609109ca11bf5597e20867ac8a451d92a0a2
shivam joshi <53056412+TtvTooSmart@users.noreply.github.com>
2022-07-17 11:34:32 +0530

    added challenges

--- rev 3-6/script.py
+++ rev 3-6/script.py
@@ -1,6 +1,7 @@
 import socketserver
 import time
 import threading
+import random
 
 
 class Service(socketserver.BaseRequestHandler):
@@ -8,8 +9,22 @@ class Service(socketserver.BaseRequestHandler):
         # pass
         print("someone connected!")
 
-        entered = self.receive()
-        self.send("you said" + repr(entered))
+        # entered = self.receive()
+        # self.send("you said" + repr(entered))
+
+        handle = open('/dev/')
+        offset = random.randint ( 60, 120 )
+        starting_time = int(time.time())
+        
+
+        while(True):
+            if (int(time.time()) != starting_time + offset ):
+                self.send (handle.read(1), newline = False)
+            else:
+                flag = 'NuvCTF{test_flag}'
+                self.send(flag, newline= False)
+                starting_time = int(time.time())
+                offset = random.randint ( 60, 120 )
 
     def send(self, string, newline = True):
         if newline: string = string + "\n"
@@ -21,7 +36,7 @@ class Service(socketserver.BaseRequestHandler):
 
 
 
-class Threadedservice( socketserver.ThreadingMixIn, socketserver.TCPServer, socketserver.DatagramRequestHandler):
+class ThreadedService( socketserver.ThreadingMixIn, socketserver.TCPServer, socketserver.DatagramRequestHandler):
     pass
 
 def main():
@@ -31,13 +46,13 @@ def main():
     # host = '192.168.56.1'
     # host = '192.168.17.1'
     # host = '172.25.160.1'
-    # host = '0.0.0.0'
-    host = '172.25.160.1'
+    host = '0.0.0.0'
+    # host = '192.168.0.3'
 
 
     service = Service
 
-    server = Threadedservice( (host , port), service)
+    server = ThreadedService( (host , port), service)
     server.allow_reuse_address = True
 
     server_thread = threading.Thread( target = server.serve_forever)
