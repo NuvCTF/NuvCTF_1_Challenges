34a5b1e3dfa84b666d6c8fd0ed5657958c69d1fe
shivam joshi <53056412+TtvTooSmart@users.noreply.github.com>
2022-07-17 19:03:47 +0530

    added challenges

--- rev 3-6/chall babyrev/rev.c
+++ rev 3-6/chall babyrev/rev.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #define SIZE 100
 
@@ -12,8 +13,11 @@ char* base64Decoder(char encoded[], int len_str)
     int count_bits = 0;
 
     for (i = 0; i < len_str; i += 4) {
+        
         num = 0, count_bits = 0;
+
         for (j = 0; j < 4; j++) {
+
             if (encoded[i + j] != '=') {
                 num = num << 6;
                 count_bits += 6;
@@ -53,14 +57,24 @@ char* base64Decoder(char encoded[], int len_str)
     return decoded_string;
 }
  
-int main()
-{
+int main() {
     char encoded_string[] = "TnV2Q1RGe2cwMGRfajBiX3kwdV9uMHdfdW4zcnN0NG5kX2I0czFjNV8wZl9yM3YzcnMzXzNuZzFuMzNyMW5nfQ==";
     int len_str = sizeof(encoded_string) / sizeof(encoded_string[0]);
- 
+    
+    char enc_str[] = "aGVsbG9tYXRjaGE=";
+    int len_strn = sizeof(enc_str) / sizeof(enc_str[0]); 
+    
     len_str -= 1;
- 
-    printf("Decoded_string : %s\n", base64Decoder(encoded_string, len_str));
- 
+    char ss[30];
+    
+    printf("Please Enter the Password \n");
+    scanf("%s",ss);
+    
+        if (strcmp(ss, "hellomatcha") == 0) {
+            printf("Decoded_string : %s\n", base64Decoder(encoded_string, len_str));
+        }
+        else {
+            printf("Wrong Password");
+        } 
     return 0;
 }
