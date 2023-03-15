93922d3924fbdfed9f98ab06481f8a0469bbe58a
shivam joshi <Shivamj8866@gmail.com>
2023-03-15 22:33:21 +0530

    added challenges

--- misc 3-6/chall pass crack/sol.py
+++ misc 3-6/chall pass crack/sol.py
@@ -17,6 +17,9 @@ for i in range(1,399):
         img = cv2.imread(image)
         detect = cv2.QRCodeDetector()
         value, points, straight_qrcode = detect.detectAndDecode(img)
+        # value_2 = "NuvCTF{HAHAHA_FAKE_FLAG" + i + "}"
+        # if (value != value_2):
+        #     print(value)
         print(value)
     except:
         print(str(i) + ": error")
